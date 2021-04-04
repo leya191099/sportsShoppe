97d63ca8e8338377348921b47b9ce3d140415c48
pomo <pomo@LAPTOP-SQRTEBD2>
2021-04-04 23:49:37 +0530

    test

--- SportsShoppe/src/test/java/com/cp/sports/Service/IUserServiceTest.java
+++ SportsShoppe/src/test/java/com/cp/sports/Service/IUserServiceTest.java
@@ -1,24 +1,35 @@
 package com.cp.sports.Service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.cp.sports.Entity.User;
 
+@SpringBootTest
 public class IUserServiceTest {
-	@Autowired
-	IUserService userService;
-	User user1=new User("CAPG123","123password","customer");
-	User user2=new User("CAPG125","125password","customer");
-
-	@Test
-	void addUser() {
-		
-		assertEquals("customer",userService.addUser(user1).getRole().toString());
-	}
+@Autowired
+private IUserService iUserService;
+User user1=new User("12345", "admin", "admin");
+User user2=new User("12345", "teju", "admin");
+@Test
+void addUserTest() {
 	
-	
+	assertEquals("admin",iUserService.addUser(user1).getPassword());
+}
+@Test
+void updateUserTest() {
+	assertEquals("teju",iUserService.updateUser(user2).getPassword());
+}
+@Test
+void signInTest() {
+	assertEquals("tejuadmin",iUserService.changePassword("12345", "teju","tejuadmin").getPassword());
+}
+@Test
+void deleteTest() {
+	assertNull(iUserService.deleteUser("12345"));
+}
 }
