29f29d3c306974f1075749033fdd0d4de63bf708
pomo <pomo@LAPTOP-SQRTEBD2>
2021-04-05 13:17:15 +0530

    comit3

--- SportsShoppe/src/test/java/com/cp/sports/Service/IProductServiceTest.java
+++ SportsShoppe/src/test/java/com/cp/sports/Service/IProductServiceTest.java
@@ -13,45 +13,50 @@ import com.cp.sports.Entity.Product;
 
 @SpringBootTest
 public class IProductServiceTest {
-		@Autowired
-		private IProductService productservice;
+	@Autowired
+	private IProductService productservice;
 
-		
+	Product product = new Product("1234", "football shoes", "shoes", "very good shoe", "adidas", "brown", "6", "655",
+			15, 410, true, LocalDate.parse("2021-03-31"));
 
-Product product=new Product("1234", "football shoes", "shoes", "very good shoe", "adidas", "brown", "6", "655", 15, 410, true, LocalDate.parse("2021-03-31"));
-		
-		@Test
-		void updateProductTest() {
-			System.out.println(productservice.updateProduct("1234", product));
-			assertEquals("Product [productId=1234, productName=football shoes, category=shoes, description=very good shoe, brand=adidas, color=brown, size=6, mrp=655, discount=15, priceAfterDiscount=410.0, inStock=true, estimatedDelivery=2021-03-31]", productservice.updateProduct("1234", product).toString());
-		}
-		
-		@Test
-		void addProductTest() {
-			Product product=new Product("1235", "football shoes", "shoes", "very good shoe", "adidas", "brown", "6", "655", 15, 410, true, LocalDate.parse("2021-03-31"));
-productservice.addProduct(product);
-assertNotNull(product.getProductId());
-			System.out.println(productservice.getProduct("1235"));
+	@Test
+	void updateProductTest() {
+		System.out.println(productservice.updateProduct("1234", product));
+		assertEquals(
+				"Product [productId=1234, productName=football shoes, category=shoes, description=very good shoe, brand=adidas, color=brown, size=6, mrp=655, discount=15, priceAfterDiscount=410.0, inStock=true, estimatedDelivery=2021-03-31]",
+				productservice.updateProduct("1234", product).toString());
+	}
+
+	@Test
+	void addProductTest() {
+		Product product = new Product("1235", "football shoes", "shoes", "very good shoe", "adidas", "brown", "6",
+				"655", 15, 410, true, LocalDate.parse("2021-03-31"));
+		productservice.addProduct(product);
+		assertNotNull(product.getProductId());
+		System.out.println(productservice.getProduct("1235"));
 //			System.out.println(productservice.getAllProduct().size());
-		}
-		@Test
-		void getIdTest1() {
+	}
+
+	@Test
+	void getIdTest1() {
 
 		assertEquals("football shoes", productservice.getProduct("1234").getProductName().toString());
-		}
-		
-		@Test
-		void getPriceTest() {
+	}
+
+	@Test
+	void getPriceTest() {
 
 		assertEquals(410, productservice.getProduct("1234").getPriceAfterDiscount());
-		}
-		@Test
-		void getColorTest() {
-	assertEquals("brown", productservice.getProduct("1234").getColor());
-		}
-		@Test
-		void getSizeTest() {
-		assertEquals("6", productservice.getProduct("1234").getSize());
-		}
-
 	}
+
+	@Test
+	void getColorTest() {
+		assertEquals("brown", productservice.getProduct("1234").getColor());
+	}
+
+	@Test
+	void getSizeTest() {
+		assertEquals("6", productservice.getProduct("1234").getSize());
+	}
+
+}
